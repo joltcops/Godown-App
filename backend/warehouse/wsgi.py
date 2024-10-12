7962de0446545f2298ace95bd24e629fc2721df1
joltcops <pracheta@kgpian.iitkgp.ac.in>
2024-10-12 14:14:31 +0530

    Im done

--- backend/warehouse/wsgi.py
+++ backend/warehouse/wsgi.py
@@ -1,5 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import sys
+
+# Add the backend directory to the system path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.warehouse.settings')
 

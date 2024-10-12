70c23e938fcd2b95a268d2f6a175fb6bc5babf13
joltcops <pracheta@kgpian.iitkgp.ac.in>
2024-10-12 14:33:15 +0530

    Final changes, dockerisation done

--- backend/warehouse/wsgi.py
+++ backend/warehouse/wsgi.py
@@ -1,11 +1,16 @@
+"""
+WSGI config for warehouse project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
-import sys
 
-# Add the backend directory to the system path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.warehouse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'warehouse.settings')
 
 application = get_wsgi_application()
